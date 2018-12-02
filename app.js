03a1ddf3daea5d7a1da8a44db9750792120c0244
gokhanDerala <gderala@gmail.com>
2018-12-03 00:44:47 +0300

    finish the logic

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const comptScore = 0;
+let userScore = 0;
+let comptScore = 0;
 const userScore_span = document.getElementById("user-score");
 const comptScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_div = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
@@ -15,23 +15,49 @@ function getComputerChoices(){
 	return choices[randomNumber];
 }
 
+function convert(letter){
+	if(letter== "r") return "Rock";
+	if(letter== "p") return "Paper";
+	if(letter== "s") return "Scissors";
+}
+
+function win(user, computer){
+	userScore++;
+	userScore_span.innerHTML = userScore;
+	comptScore_span.innerHTML = comptScore;
+	result_div.innerHTML = `${convert(user)} beats ${convert(computer)}. You win!`;
+}
+
+function lose(user, computer){
+	comptScore++;
+	userScore_span.innerHTML = userScore;
+	comptScore_span.innerHTML = comptScore;
+	result_div.innerHTML = `${convert(user)} loses to ${convert(computer)}. You lose!`;
+}
+
+function draw(user, computer){
+	userScore_span.innerHTML = userScore;
+	comptScore_span.innerHTML = comptScore;
+	result_div.innerHTML = `${convert(user)} equals to ${convert(computer)}. It's a draw.`;
+}
+
 function game(userChoice) {
 	const computerChoice = getComputerChoices();
 	switch(userChoice + computerChoice){
 		case "rs":
 		case "pr":
 		case "sp":
-			console.log("User Wins");
+			win(userChoice, computerChoice);
 			break;
 		case "rp":
 		case "ps":
 		case "sr":
-			console.log("User loses");
+			lose(userChoice, computerChoice);
 			break;	
 		case "rr":
 		case "pp":
 		case "ss":
-			console.log("DRAW");
+			draw(userChoice, computerChoice);
 			break;
 	}
 }
