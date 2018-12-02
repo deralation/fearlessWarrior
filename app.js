497969171a8edfdfa5b99fcbf622a528b7a26330
gokhanDerala <gderala@gmail.com>
2018-12-03 00:19:06 +0300

    logic of game

--- app.js
+++ app.js
@@ -7,3 +7,47 @@ const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+
+
+function getComputerChoices(){
+	const choices = ["r","p","s"];
+	const randomNumber = Math.floor(Math.random() * 3);
+	return choices[randomNumber];
+}
+
+function game(userChoice) {
+	const computerChoice = getComputerChoices();
+	switch(userChoice + computerChoice){
+		case "rs":
+		case "pr":
+		case "sp":
+			console.log("User Wins");
+			break;
+		case "rp":
+		case "ps":
+		case "sr":
+			console.log("User loses");
+			break;	
+		case "rr":
+		case "pp":
+		case "ss":
+			console.log("DRAW");
+			break;
+	}
+}
+
+function main() {
+	rock_div.addEventListener("click",function(){
+		game("r");
+	});
+
+	paper_div.addEventListener("click",function(){
+		game("p");
+	});
+
+	scissors_div.addEventListener("click",function(){
+		game("s");
+	});	
+}
+
+main();
